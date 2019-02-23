58965c9120a495ab19556754c5180af14abe4ac2
Reinhard X. Fürst <rexfue@gmail.com>
2019-02-23 14:52:29 +0100

    Anzeige dazu

--- LoraGeigerTTN_MKRWAN/LoraGeigerTTN_MKRWAN.ino
+++ LoraGeigerTTN_MKRWAN/LoraGeigerTTN_MKRWAN.ino
@@ -3,6 +3,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+
 #define MAXCNT 100 
 #define CalFactor 1 
 
@@ -13,21 +14,21 @@ LoRaModem modem;
 #define SCREEN_HEIGHT 64
 Adafruit_SSD1306 display(SCREEN_WIDTH,SCREEN_HEIGHT,&Wire,OLED_RESET);
 
-//#include "arduino_secrets.h"
-// Please enter your sensitive data in the Secret tab or arduino_secrets.h
-String appEui = "70B3D57ED0017A98";
-String appKey = "61989423979892487C2597C2BAA3DB20";
+// Please enter your sensitive data in appkeys.h
+#include "appkeys.h"
 
 
 CayenneLPP lpp(51);
 
 volatile int counter = 0; 
 unsigned long oldTime = 0; 
+volatile char gotit = 0;
+unsigned long lastsec;
 
 void writeText(String txt, int x, int y, int size) {
   display.setTextSize(size);      // Normal 1:1 pixel scale
-  display.setTextColor(WHITE); // Draw white text
-  display.setCursor(x, y);     // Start at top-left corner
+  display.setTextColor(WHITE,BLACK);    // Draw white text
+  display.setCursor(x, y);        // Start at top-left corner
   display.print(txt);
   display.display();
 }
@@ -44,7 +45,7 @@ void setup()
   writeText("     Geigerzaehler",0,0,1);
   
   pinMode(LED_BUILTIN, OUTPUT);        //sign of live
-/*  for (int i=1; i<=10; i++) {           
+  /* for (int i=1; i<=10; i++) {           
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
     delay(1000);                       // wait for a second
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
@@ -52,7 +53,7 @@ void setup()
     // wait for a second
     Serial.println(10-i);
   }
-*/  
+  */
   // change this to your regional band (eg. US915, AS923, ...)
   if (!modem.begin(EU868)) {
     Serial.println("Failed to start module");
@@ -71,22 +72,25 @@ void setup()
   int connected = modem.joinOTAA(appEui, appKey);
   if (!connected) {
     Serial.println("Something went wrong; are you indoor? Move near a window and retry");
+    writeText("No LoRa   ",65,40,1);
  //   while (1) {}
   }
   else
     Serial.println("Successfully joined");
-    writeText("Joined !",65,40,1);
+    writeText("Joined ! ",65,40,1);
   // Set poll interval to 60 secs.
   modem.minPollInterval(60);
   // NOTE: independently by this setting the modem will
   // not allow to send more than one message every 2 minutes,
   // this is enforced by firmware and can not be changed.
 
-  delay(2000);
+  delay(3000);
   display.clearDisplay();
   writeText("     Geigerzaehler",0,0,1);
   Serial.println("Start waiting for geiger pulses ...");
   attachInterrupt(digitalPinToInterrupt(0), count, FALLING); 
+
+  lastsec = millis();
 
 }
 
@@ -96,7 +100,19 @@ void loop()
   unsigned long dt; 
   float rate; 
   int err;
-
+  if (gotit) {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(10);                         // wait for 10msec
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  gotit=0;
+  }
+  time = millis();
+  if ((time - lastsec) >= 1000) {
+    lastsec = time;
+    char cntstr[4];
+    sprintf(cntstr,"%03d",counter);
+    writeText(String(cntstr),0,55,1);
+  }
   if (counter == MAXCNT) { 
     detachInterrupt(digitalPinToInterrupt(0)); 
     time = millis(); 
@@ -104,11 +120,8 @@ void loop()
     rate = (float)MAXCNT*60.0*1000.0/(float)dt/CalFactor; 
     Serial.println(round(rate));
     
-    display.clearDisplay();
-    writeText("     Geigerzaehler",0,0,1);
     writeText(String(rate),10,30,2);
     writeText("cpm",100,50,1);
-    Serial.println("Sent to LCD");
     
     lpp.reset();
     lpp.addLuminosity(1, round(rate));
@@ -152,4 +165,5 @@ void loop()
 void count() 
 { 
   counter++; 
+  gotit=1;
 }
